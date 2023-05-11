e09a559345502ab60504e0d111befe750c7ffa21
Jessica Jiang <jessicaj2947@gmail.com>
2023-05-12 11:08:03 +1200

    correctly print welcome message

--- src/main/java/nz/ac/auckland/se281/Morra.java
+++ src/main/java/nz/ac/auckland/se281/Morra.java
@@ -4,11 +4,16 @@ import nz.ac.auckland.se281.Main.Difficulty;
 
 public class Morra {
 
-  public Morra() {}
+  public Morra() {
+  }
 
-  public void newGame(Difficulty difficulty, int pointsToWin, String[] options) {}
+  public void newGame(Difficulty difficulty, int pointsToWin, String[] options) {
+    MessageCli.WELCOME_PLAYER.printMessage(options[0]);
+  }
 
-  public void play() {}
+  public void play() {
+  }
 
-  public void showStats() {}
+  public void showStats() {
+  }
 }
