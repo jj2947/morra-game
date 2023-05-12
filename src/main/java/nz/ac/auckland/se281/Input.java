2f6f3ecfb845dca95c69ba049b2a76d7702a8cf3
Jessica Jiang <jessicaj2947@gmail.com>
2023-05-12 13:06:05 +1200

    moved isvalidinput method into input class

--- src/main/java/nz/ac/auckland/se281/Input.java
+++ src/main/java/nz/ac/auckland/se281/Input.java
@@ -1,20 +1,15 @@
 package nz.ac.auckland.se281;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Input {
-    
+
     private boolean isValid;
     private String fingers;
     private String sum;
-    private List<String> InputList;
 
     public Input() {
         isValid = false;
         fingers = "";
         sum = "";
-        InputList = new ArrayList<>();
     }
 
     public boolean isValid() {
@@ -41,7 +36,55 @@ public class Input {
         this.sum = sum;
     }
 
-    public List<String> getInputList() {
-        return InputList;
+    public void isValidInput(String input) {
+
+        int spaceIndexNum = 0;
+
+        if (!Character.isDigit(input.charAt(0))) {
+            setValid(false);
+            return;
+        }
+
+        for (int i = 1; i < input.length(); i++) {
+            if (input.charAt(i) == ' ') {
+                spaceIndexNum = i;
+                break;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < spaceIndexNum; i++) {
+            if (Character.isDigit(input.charAt(i))) {
+                sb.append(input.charAt(i));
+            } else {
+                setValid(false);
+                return;
+            }
+        }
+
+        StringBuilder sb2 = new StringBuilder();
+
+        for (int i = spaceIndexNum + 1; i < input.length(); i++) {
+            if (Character.isDigit(input.charAt(i))) {
+                sb2.append(input.charAt(i));
+            } else {
+                setValid(false);
+                return;
+            }
+        }
+
+        String fingers = sb.toString();
+        String sum = sb2.toString();
+
+        if (Integer.parseInt(fingers) < 1 || Integer.parseInt(fingers) > 5 || Integer.parseInt(sum) < 1
+                || Integer.parseInt(sum) > 10) {
+            setValid(false);
+            return;
+        }
+
+        setValid(true);
+        setFingers(fingers);
+        setSum(sum);
     }
 }
--- src/main/java/nz/ac/auckland/se281/Morra.java
+++ src/main/java/nz/ac/auckland/se281/Morra.java
@@ -23,71 +23,19 @@ public class Morra {
 
     MessageCli.ASK_INPUT.printMessage();
     String input = Utils.scanner.nextLine();
-    Input inputInfo = isValidInput(input);
+    Input inputInfo = new Input();
+    inputInfo.isValidInput(input);
+    boolean isValid = inputInfo.isValid();
 
-    while (!inputInfo.isValid()) {
+    while (!isValid) {
       MessageCli.INVALID_INPUT.printMessage();
       MessageCli.ASK_INPUT.printMessage();
       input = Utils.scanner.nextLine();
-      inputInfo = isValidInput(input);
+      inputInfo.isValidInput(input);
+      isValid = inputInfo.isValid();
     }
 
     MessageCli.PRINT_INFO_HAND.printMessage(name, inputInfo.getFingers(), inputInfo.getSum());
-  }
-
-  private Input isValidInput(String input) {
-
-    int spaceIndexNum = 0;
-
-    Input inputObj = new Input();
-
-    if (!Character.isDigit(input.charAt(0))) {
-      inputObj.setValid(false);
-      return inputObj;
-    }
-
-    for (int i = 1; i < input.length(); i++) {
-      if (input.charAt(i) == ' ') {
-        spaceIndexNum = i;
-        break;
-      }
-    }
-
-    StringBuilder sb = new StringBuilder();
-
-    for (int i = 0; i < spaceIndexNum; i++) {
-      if (Character.isDigit(input.charAt(i))) {
-        sb.append(input.charAt(i));
-      } else {
-        inputObj.setValid(false);
-        return inputObj;
-      }
-    }
-
-    StringBuilder sb2 = new StringBuilder();
-
-    for (int i = spaceIndexNum + 1; i < input.length(); i++) {
-      if (Character.isDigit(input.charAt(i))) {
-        sb2.append(input.charAt(i));
-      } else {
-        inputObj.setValid(false);
-        return inputObj;
-      }
-    }
-
-    String fingers = sb.toString();
-    String sum = sb2.toString();
-
-    if (Integer.parseInt(fingers) < 1 || Integer.parseInt(fingers) > 5 || Integer.parseInt(sum) < 1
-        || Integer.parseInt(sum) > 10) {
-          inputObj.setValid(false);
-          return inputObj;
-    }
-
-    inputObj.setValid(true);
-    inputObj.setFingers(fingers);
-    inputObj.setSum(sum);
-    return inputObj;
   }
 
   public void showStats() {
