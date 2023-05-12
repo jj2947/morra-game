9f181dc0e25e4d073e19f13d9ff4a336499e414e
Jessica Jiang <jessicaj2947@gmail.com>
2023-05-12 13:45:19 +1200

    made an AI class

---
+++ src/main/java/nz/ac/auckland/se281/AI.java
@@ -0,0 +1,20 @@
+package nz.ac.auckland.se281;
+
+import nz.ac.auckland.se281.Strategies.Strategy;
+
+public class AI {
+    
+    private Strategy strategy;
+    
+    public AI(Strategy strategy){
+        this.strategy = strategy;
+    }
+
+    public void setStrategy() {
+        this.strategy = strategy;
+    }
+    
+    public Input play(){
+        return strategy.strategy();
+    }
+}
