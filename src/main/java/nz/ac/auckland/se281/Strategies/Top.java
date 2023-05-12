445395fb4aed8c53bee93c00ef186f83a533923e
Jessica Jiang <jessicaj2947@gmail.com>
2023-05-12 16:43:38 +1200

    added getTop method that gets the most used fingers

--- src/main/java/nz/ac/auckland/se281/Strategies/Top.java
+++ src/main/java/nz/ac/auckland/se281/Strategies/Top.java
@@ -3,17 +3,42 @@ package nz.ac.auckland.se281.Strategies;
 import java.util.List;
 
 import nz.ac.auckland.se281.Input;
+import nz.ac.auckland.se281.Utils;
 
 public class Top implements Strategy {
 
-    public Top(List<Integer> fingers) {
+    List<Integer> fingers;
+    int top;
 
+    public Top(List<Integer> fingers) {
+        this.fingers = fingers;
     }
+
+    private int getTop() {
+        int count;
+        int prevCount = 0;
+        int top = fingers.get(0);
+
+        for (int i = 0; i < fingers.size(); i++) {
+            count = 0;
+            for (int j = 0; j < fingers.size(); j++) {
+                if (fingers.get(i) == fingers.get(j)) {
+                    count = prevCount;
+                    count++;
+
+                    if (count > prevCount) {
+                        top = fingers.get(i);
+                    }
+                }
+            }
+        }
+        return top;
+    }
+
     @Override
     public Input strategy() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'strategy'");
+        int fingers = Utils.getRandomNumber(1, 5);
+        
     }
-    
-    
+
 }
