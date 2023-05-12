bc07e274dffa3726e80078b9811bb0d74c5ef966
Jessica Jiang <jessicaj2947@gmail.com>
2023-05-12 16:51:42 +1200

    implemented the Top strategy using my getTop method and also fixed bugs in getTop method

--- src/main/java/nz/ac/auckland/se281/Strategies/Top.java
+++ src/main/java/nz/ac/auckland/se281/Strategies/Top.java
@@ -3,6 +3,7 @@ package nz.ac.auckland.se281.Strategies;
 import java.util.List;
 
 import nz.ac.auckland.se281.Input;
+import nz.ac.auckland.se281.MessageCli;
 import nz.ac.auckland.se281.Utils;
 
 public class Top implements Strategy {
@@ -16,19 +17,20 @@ public class Top implements Strategy {
 
     private int getTop() {
         int count;
-        int prevCount = 0;
+        int highestCount = 0;
         int top = fingers.get(0);
 
         for (int i = 0; i < fingers.size(); i++) {
             count = 0;
             for (int j = 0; j < fingers.size(); j++) {
                 if (fingers.get(i) == fingers.get(j)) {
-                    count = prevCount;
                     count++;
 
-                    if (count > prevCount) {
+                    if (count > highestCount) {
                         top = fingers.get(i);
+                        highestCount = count;
                     }
+                    
                 }
             }
         }
@@ -38,7 +40,14 @@ public class Top implements Strategy {
     @Override
     public Input strategy() {
         int fingers = Utils.getRandomNumber(1, 5);
-        
+        int sum = fingers + getTop();
+        Input input = new Input("Jarvis");
+        input.setFingers(String.valueOf(fingers));
+        input.setSum(String.valueOf(sum));
+        input.setValid(true);
+        MessageCli.PRINT_INFO_HAND.printMessage(input.getName(), String.valueOf(input.getFingers()),
+                String.valueOf(input.getSum()));
+        return input;
     }
 
 }
