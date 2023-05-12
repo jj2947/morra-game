c9e42f5c11d5bda2482cb434c62601005d034448
Jessica Jiang <jessicaj2947@gmail.com>
2023-05-12 12:20:34 +1200

    made an input class

---
+++ src/main/java/nz/ac/auckland/se281/Input.java
@@ -0,0 +1,21 @@
+package nz.ac.auckland.se281;
+
+public class Input {
+    
+    private String name;
+    private String fingers;
+    private String sum;
+
+    public Input(String name, String fingers, String sum) {
+        this.name = name;
+        this.fingers = fingers;
+        this.sum = sum;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+
+}
