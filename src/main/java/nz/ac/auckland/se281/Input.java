a82b35a2b7e3819b0f48af69e7943df953cca4c7
Jessica Jiang <jessicaj2947@gmail.com>
2023-05-12 12:46:55 +1200

    implemented logic for getting input and making sure its valid and added more methods to Input class

--- src/main/java/nz/ac/auckland/se281/Input.java
+++ src/main/java/nz/ac/auckland/se281/Input.java
@@ -1,21 +1,47 @@
 package nz.ac.auckland.se281;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Input {
     
-    private String name;
+    private boolean isValid;
     private String fingers;
     private String sum;
+    private List<String> InputList;
 
-    public Input(String name, String fingers, String sum) {
-        this.name = name;
+    public Input() {
+        isValid = false;
+        fingers = "";
+        sum = "";
+        InputList = new ArrayList<>();
+    }
+
+    public boolean isValid() {
+        return isValid;
+    }
+
+    public String getFingers() {
+        return fingers;
+    }
+
+    public String getSum() {
+        return sum;
+    }
+
+    public void setValid(boolean isValid) {
+        this.isValid = isValid;
+    }
+
+    public void setFingers(String fingers) {
         this.fingers = fingers;
+    }
+
+    public void setSum(String sum) {
         this.sum = sum;
     }
 
-    public String getName() {
-        return name;
+    public List<String> getInputList() {
+        return InputList;
     }
-
-
-
 }
--- src/main/java/nz/ac/auckland/se281/Morra.java
+++ src/main/java/nz/ac/auckland/se281/Morra.java
@@ -13,33 +13,37 @@ public class Morra {
   public void newGame(Difficulty difficulty, int pointsToWin, String[] options) {
     name = options[0];
     MessageCli.WELCOME_PLAYER.printMessage(name);
-    roundNumber = 1;
+    roundNumber = 0;
   }
 
   public void play() {
 
-    MessageCli.START_ROUND.printMessage(String.valueOf(roundNumber));
     roundNumber++;
+    MessageCli.START_ROUND.printMessage(String.valueOf(roundNumber));
 
     MessageCli.ASK_INPUT.printMessage();
     String input = Utils.scanner.nextLine();
+    Input inputInfo = isValidInput(input);
 
-    while (!isValidInput(input)) {
+    while (!inputInfo.isValid()) {
       MessageCli.INVALID_INPUT.printMessage();
       MessageCli.ASK_INPUT.printMessage();
       input = Utils.scanner.nextLine();
-      isValidInput(input);
+      inputInfo = isValidInput(input);
     }
 
-    MessageCli.PRINT_INFO_HAND.printMessage(name, );
+    MessageCli.PRINT_INFO_HAND.printMessage(name, inputInfo.getFingers(), inputInfo.getSum());
   }
 
-  private boolean isValidInput(String input) {
+  private Input isValidInput(String input) {
 
     int spaceIndexNum = 0;
 
+    Input inputObj = new Input();
+
     if (!Character.isDigit(input.charAt(0))) {
-      return false;
+      inputObj.setValid(false);
+      return inputObj;
     }
 
     for (int i = 1; i < input.length(); i++) {
@@ -55,7 +59,8 @@ public class Morra {
       if (Character.isDigit(input.charAt(i))) {
         sb.append(input.charAt(i));
       } else {
-        return false;
+        inputObj.setValid(false);
+        return inputObj;
       }
     }
 
@@ -65,7 +70,8 @@ public class Morra {
       if (Character.isDigit(input.charAt(i))) {
         sb2.append(input.charAt(i));
       } else {
-        return false;
+        inputObj.setValid(false);
+        return inputObj;
       }
     }
 
@@ -74,10 +80,14 @@ public class Morra {
 
     if (Integer.parseInt(fingers) < 1 || Integer.parseInt(fingers) > 5 || Integer.parseInt(sum) < 1
         || Integer.parseInt(sum) > 10) {
-          return false;
+          inputObj.setValid(false);
+          return inputObj;
     }
 
-    return true;
+    inputObj.setValid(true);
+    inputObj.setFingers(fingers);
+    inputObj.setSum(sum);
+    return inputObj;
   }
 
   public void showStats() {
